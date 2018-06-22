37087b8b9f83b253ee4a5375050a18cb1cd490e5
Konisko Bandyopadhyay <30558121+Jakoban2411@users.noreply.github.com>
2018-06-22 14:53:36 +0530

    Created a C++ AI Task

---
+++ Source/GetOUT/Public/ChooseTheNextWay.h
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTTaskNode.h"
+#include "ChooseTheNextWay.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GETOUT_API UChooseTheNextWay : public UBTTaskNode
+{
+	GENERATED_BODY()
+	
+		virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
+
+	
+	
+};
